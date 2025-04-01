cba02c455831907d06f19af2b5af9ad7d4aaa940
YAZZYD <islam.yazid.ben@gmail.com>
2025-04-01 21:35:20 +0100

    Add preprocessed data handling and update main application flow

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import os
+import argparse
+import pandas as pd
 from preprocessing.process_captures import process_captures
-from preprocessing.handlers.load_labels import load_labels
 import warnings
 warnings.filterwarnings('ignore')
 
-def main():
+def process_data():
     captured_dir = './dataset/captured'
     description = './dataset/description.csv'
     if not os.path.exists(captured_dir):
         print(f"dir '{captured_dir}' not found")
         return
-    df=process_captures(captured_dir,description)
-    print(df)
+    return process_captures(captured_dir,description)
 
+def read_preprocessed_data():
+    preprocessed_file = './dataset/preprocessed/preprocessed_data.csv'
+    return pd.read_csv(preprocessed_file)
+
+def main():
+    parser = argparse.ArgumentParser(description="IDS (Intrusion Detection System)")
+    parser.add_argument('--pre-processed',action='store_true',help="Use preprocessed data if available")
+    args= parser.parse_args()
+    df=None
+    if args.pre_processed:
+        df=read_preprocessed_data()
+        print(df)
+    else:
+        df= process_data()
+   
 
 if __name__ == "__main__":
     main()
--- makefile
+++ makefile
@@ -1,2 +1,4 @@
-run:
+run-without-preprocess:
+	python3 main.py --pre-processed
+run-with-preprocess:
 	python3 main.py
